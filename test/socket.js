adcf7bc652b4f7a20897ad6f00c05034b834409e
hc <hheennrryy@gmail.com>
2013-03-20 11:07:41 -0400

    restructure

--- test/socket.js
+++ test/socket.js
@@ -1,10 +1,18 @@
-
+var PSocket = require('../lib/socket')
+  , net = require('net')
+  , Socket = net.Socket
+  , assert = require('assert');
 
 describe('Socket', function() {
 
+  it('exposes socket events', function() {
+    var p = new Pool([{host: '127.0.0.1', port: 3001}]);
+    var sock = new Socket();
+  });
+
   describe('release', function() {
     // after the socket is deemed good
-    it('the socket back into the pool');
+    it('socket back into the pool');
   });
 
 });
