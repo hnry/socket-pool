b2e5bb9b636c8a85ec7930ce8b148ba205a7cb70
hc <hheennrryy@gmail.com>
2013-03-20 22:30:32 -0400

    socket implementation

--- test/socket.js
+++ test/socket.js
@@ -3,11 +3,28 @@ var PSocket = require('../lib/socket')
   , Socket = net.Socket
   , assert = require('assert');
 
+
+var ps;
+
 describe('Socket', function() {
 
-  it('exposes socket events', function() {
-    var p = new Pool([{host: '127.0.0.1', port: 3001}]);
-    var sock = new Socket();
+  before(function() {
+    var s = new Socket();
+    ps = new PSocket(s);
+  })
+
+  it('_socket events bubble up', function(done) {
+    assert.strictEqual(ps._socket._events.error.length, 1);
+    ps.on('error', function(err) {
+      done();
+    });
+    ps.connect();
+  });
+
+  it('exposes _socket properties and functions', function() {
+    assert.equal(typeof ps.write, 'function');
+    assert.equal(typeof ps.connect, 'function');
+    assert.equal(typeof ps.end, 'function');
   });
 
   describe('release', function() {
